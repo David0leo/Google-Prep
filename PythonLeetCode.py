effd7aa02d341ea1bb6c213b09a3ef0963adaf4b
David0leo <dla2@ualberta.ca>
2016-10-17 19:01:59 -0600

    Update PythonLeetCode.py

--- PythonLeetCode.py
+++ PythonLeetCode.py
@@ -16,4 +16,75 @@ def twoSum(A, k):
             return([ D[k-n] , i] )
         D[n] = i
 
-def
+########### 2. Add Two Numbers ###########
+# -- Given two linked list representations
+#    of integers a, b >= 0 stored in 
+#    reverse order, return a+b as a linked
+#    list in the same form as a and b.
+# -- Ex. 1024 represented as follows:
+#    |4| -> |2| -> |0| -> |1|
+
+def addTwoNumbers(l1, l2):
+    # Space saving, but slower
+    # Modifies list l1 and sort of
+    # merges the lists in some cases
+    carry = 0
+        front = l1
+        back = l1.next
+        while l1 and l2:
+            l1.val += l2.val + carry
+            if l1.val > 9:
+                carry = 1
+                l1.val -= 10
+            else:
+                carry = 0
+            back = l1
+            l1 = l1.next
+            l2 = l2.next
+        #now l2 could be longer than l1
+        if l2:
+            back.next = l2
+            while carry and l2:
+                l2.val += carry
+                if l2.val > 9:
+                    carry = 1
+                    l2.val -= 10
+                else:
+                    carry = 0
+                back = l2
+                l2 = l2.next
+        else:
+            while carry and l1:
+                l1.val += carry
+                if l1.val > 9:
+                    carry = 1
+                    l1.val -= 10
+                else:
+                    carry = 0
+                back = l1
+                l1 = l1.next
+        if carry:
+            back.next = ListNode(1)
+        return(front)
+    
+def addTwoNumbers(l1, l2):
+    #faster, creates new list
+    head = ListNode(None)
+    cur = None
+    carry = 0
+    while l1 or l2 or carry:
+        sum = carry
+        if l1:
+            sum += l1.val
+            l1 = l1.next
+        if l2:
+            sum += l2.val
+            l2 = l2.next
+        carry = sum/10
+        if cur == None:
+            head.val = sum%10
+            cur = head
+        else:
+            cur.next = ListNode(sum%10)
+            cur = cur.next
+    return(head)
